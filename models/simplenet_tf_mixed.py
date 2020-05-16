Fixed mixed precision tests and improve mixed precision efficiency

models/simplenet_tf_mixed.py
@@ -4,8 +4,8 @@ from tf_deconv_mixed_prec import FastDeconv1D, FastDeconv2D, ChannelDeconv1D, Ch
 
 class SimpleNet1D(tf.keras.Model):
 
-    def __init__(self, num_classes, num_channels=64, groups=1, channel_deconv_loc="pre", blocks=64):
-        super().__init__()
+    def __init__(self, num_classes, num_channels=64, groups=1, channel_deconv_loc="pre", blocks=64, **kwargs):
+        super().__init__(**kwargs)
 
         self.num_channels = num_channels
         self.num_classes = num_channels
@@ -13,16 +13,17 @@ class SimpleNet1D(tf.keras.Model):
 
         self.conv1 = FastDeconv1D(3, num_channels, kernel_size=3, stride=2,
                                   padding='same', activation='relu', groups=1,
-                                  n_iter=5, momentum=0.1, block=blocks)
+                                  n_iter=5, momentum=0.1, block=blocks, **kwargs)
 
         self.conv2 = FastDeconv1D(num_channels, num_channels, kernel_size=3, stride=2,
                                   padding='same', activation='relu', groups=groups,
-                                  n_iter=5, momentum=0.1, block=blocks)
+                                  n_iter=5, momentum=0.1, block=blocks, **kwargs)
 
-        self.final_conv = ChannelDeconv1D(block=num_channels, momentum=0.1)
+        self.final_conv = ChannelDeconv1D(block=num_channels, momentum=0.1, **kwargs)
 
-        self.gap = tf.keras.layers.GlobalAveragePooling1D()
-        self.clf = tf.keras.layers.Dense(num_classes, activation='softmax')
+        self.gap = tf.keras.layers.GlobalAveragePooling1D(**kwargs)
+        self.clf = tf.keras.layers.Dense(num_classes, activation=None, **kwargs)
+        self.softmax = tf.keras.layers.Activation('softmax', dtype='float32')
 
     def call(self, inputs, training=None, mask=None):
         x = self.conv1(inputs, training=training)
@@ -36,14 +37,15 @@ class SimpleNet1D(tf.keras.Model):
             x = self.final_conv(x, training=training)
 
         x = self.clf(x)
+        x = self.softmax(x)
 
         return x
 
 
 class SimpleNet2D(tf.keras.Model):
 
-    def __init__(self, num_classes, num_channels=64, groups=1, channel_deconv_loc="pre", blocks=64):
-        super().__init__()
+    def __init__(self, num_classes, num_channels=64, groups=1, channel_deconv_loc="pre", blocks=64, **kwargs):
+        super().__init__(**kwargs)
 
         self.num_channels = num_channels
         self.num_classes = num_channels
@@ -51,16 +53,17 @@ class SimpleNet2D(tf.keras.Model):
 
         self.conv1 = FastDeconv2D(3, num_channels, kernel_size=(3, 3), stride=(2, 2),
                                   padding='same', activation='relu', groups=1,
-                                  n_iter=5, momentum=0.9, block=blocks)
+                                  n_iter=5, momentum=0.9, block=blocks, **kwargs)
 
         self.conv2 = FastDeconv2D(num_channels, num_channels, kernel_size=(3, 3), stride=(2, 2),
                                   padding='same', activation='relu', groups=groups,
-                                  n_iter=5, momentum=0.9, block=blocks)
+                                  n_iter=5, momentum=0.9, block=blocks, **kwargs)
 
-        self.final_conv = ChannelDeconv2D(block=num_channels, momentum=0.1)
+        self.final_conv = ChannelDeconv2D(block=num_channels, momentum=0.1, **kwargs)
 
-        self.gap = tf.keras.layers.GlobalAveragePooling2D()
-        self.clf = tf.keras.layers.Dense(num_classes, activation='softmax')
+        self.gap = tf.keras.layers.GlobalAveragePooling2D(**kwargs)
+        self.clf = tf.keras.layers.Dense(num_classes, activation=None, **kwargs)
+        self.softmax = tf.keras.layers.Activation('softmax', dtype='float32')
 
     def call(self, inputs, training=None, mask=None):
         x = self.conv1(inputs, training=training)
@@ -74,5 +77,6 @@ class SimpleNet2D(tf.keras.Model):
             x = self.final_conv(x, training=training)
 
         x = self.clf(x)
+        x = self.softmax(x)
 
         return x

tests/test_tf_deconv_mixed_prec.py
@@ -42,14 +42,20 @@ def tf_mixed_precision(func):
 @tf_mixed_precision
 def test_fastdeconv_1d_mixed_precision(groups, channel_deconv_loc, blocks):
     """ Test 1D variant """
-    x = tf.zeros([16, 24, 3])
+    policy = mixed_precision.global_policy()
+
+    x = tf.zeros([16, 24, 3], dtype=tf.float16)
     model2 = simplenet_tf_mixed.SimpleNet1D(num_classes=10, num_channels=64, groups=groups,
-                                            channel_deconv_loc=channel_deconv_loc, blocks=blocks)
+                                            channel_deconv_loc=channel_deconv_loc, blocks=blocks,
+                                            dtype=policy)
 
     # trace the model
     model_traced2 = tf.function(model2)
 
     out = model_traced2(x, training=True)
+    assert out.shape == [16, 10]
+
+    out = model_traced2(x, training=False)
     assert out.shape == [16, 10]
 
 
@@ -60,14 +66,20 @@ def test_fastdeconv_1d_mixed_precision(groups, channel_deconv_loc, blocks):
 @tf_mixed_precision
 def test_fastdeconv_2d_mixed_precision(groups, channe_deconv_loc, blocks):
     """ Test 1D variant """
-    x = tf.zeros([16, 32, 32, 3])
+    policy = mixed_precision.global_policy()
+
+    x = tf.zeros([16, 32, 32, 3], dtype=tf.float16)
     model2 = simplenet_tf_mixed.SimpleNet2D(num_classes=10, num_channels=64, groups=groups,
-                                            channel_deconv_loc=channe_deconv_loc, blocks=blocks)
+                                            channel_deconv_loc=channe_deconv_loc, blocks=blocks,
+                                            dtype=policy)
 
     # trace the model
     model_traced2 = tf.function(model2)
 
     out = model_traced2(x, training=True)
+    assert out.shape == [16, 10]
+
+    out = model_traced2(x, training=False)
     assert out.shape == [16, 10]
 
 

tf_deconv_mixed_prec.py
@@ -133,19 +133,19 @@ class ChannelDeconv2D(tf.keras.layers.Layer):
         # step 2. calculate deconv@x1 = cov^(-0.5)@x1
         if training:
             scale_ = tf.cast(tf.shape(x1_s)[1], x1_s.dtype)
-            cov = tf.cast((tf.matmul(x1_s, tf.transpose(x1_s)) / scale_), self.block_eye.dtype) + self.eps * self.block_eye
+            cov = (tf.matmul(x1_s, tf.transpose(x1_s)) / scale_) + tf.cast(self.eps * self.block_eye, x1_s.dtype)
             deconv = isqrt_newton_schulz_autograd(cov, self.n_iter)
         else:
-            deconv = self.running_deconv
+            deconv = tf.cast(self.running_deconv, x1_s.dtype)
 
         if self.num_batches_tracked == 0:
-            self.running_deconv.assign(deconv)
+            self.running_deconv.assign(tf.cast(deconv, self.running_deconv.dtype))
 
         if training:
             running_deconv = tf.cast(self.momentum * deconv, self.running_deconv.dtype) + (1. - self.momentum) * self.running_deconv
             self.running_deconv.assign(running_deconv)
         else:
-            deconv = self.running_deconv
+            deconv = tf.cast(self.running_deconv, x.dtype)
 
         deconv = tf.cast(deconv, x1.dtype)
         x1 = tf.matmul(deconv, x1)
@@ -340,7 +340,7 @@ class FastDeconv2D(Conv):
                 X = tf.reshape(X, [-1, X_shape_[-1]])  # [N, L^2, C * K^2] -> [N * L^2, C * K^2]
 
             # 2. subtract mean
-            X = tf.cast(X, tf.float32)
+            # X = tf.cast(X, tf.float32)
             X_mean = tf.reduce_mean(X, axis=0)
             X = X - tf.expand_dims(X_mean, axis=0)
 
@@ -364,16 +364,16 @@ class FastDeconv2D(Conv):
                 deconv = isqrt_newton_schulz_autograd_batch(Cov, self.n_iter)
 
             if self.track_running_stats:
-                running_mean = self.momentum * X_mean + (1. - self.momentum) * self.running_mean
-                running_deconv = self.momentum * deconv + (1. - self.momentum) * self.running_deconv
+                running_mean = tf.cast(self.momentum * X_mean, self.running_mean.dtype) + (1. - self.momentum) * self.running_mean
+                running_deconv = tf.cast(self.momentum * deconv, self.running_deconv.dtype) + (1. - self.momentum) * self.running_deconv
 
                 # track stats for evaluation
                 self.running_mean.assign(running_mean)
                 self.running_deconv.assign(running_deconv)
 
         else:
-            X_mean = self.running_mean
-            deconv = self.running_deconv
+            X_mean = tf.cast(self.running_mean, x.dtype)
+            deconv = tf.cast(self.running_deconv, x.dtype)
 
         # 4. X * deconv * conv = X * (deconv * conv)
         if self.groups == 1:
@@ -383,7 +383,7 @@ class FastDeconv2D(Conv):
             w = tf.matmul(w, tf.cast(deconv, w.dtype))
 
             if self.use_bias:
-                b_dash = tf.matmul(w, (tf.cast(tf.expand_dims(X_mean, axis=-1), w.dtype)))
+                b_dash = tf.matmul(w, tf.cast(tf.expand_dims(X_mean, axis=-1), dtype=w.dtype))
                 b_dash = tf.reshape(b_dash, [self.filters, -1])
                 b_dash = tf.reduce_sum(b_dash, axis=1)
                 b = self.bias - b_dash
@@ -398,7 +398,7 @@ class FastDeconv2D(Conv):
             w = tf.matmul(w, tf.cast(deconv, w.dtype))
 
             if self.use_bias:
-                b_dash = tf.matmul(w, tf.cast(tf.reshape(X_mean, [-1, self.num_features, 1]), w.dtype))
+                b_dash = tf.matmul(w, tf.cast(tf.reshape(X_mean, [-1, self.num_features, 1]), dtype=w.dtype))
                 b_dash = tf.reshape(b_dash, self.bias.shape)
                 b = self.bias - b_dash
             else: